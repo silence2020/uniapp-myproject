8e501a81db2a43d3cdb48c3d279bd91f4642126f
zyb <zyb2020@qq.com>
2024-09-17 15:50:04 +0800

    公告栏&引入公共样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,19 @@
 <template>
-	<view class="content">
-		<navbar titleText="首页"></navbar>
-		<button style="margin-top: 130rpx;" @click="toSearch">ToSearch</button>
+	<view>
+		<navbar></navbar>
+		<view style="margin-top: 130rpx;">
+			<view class="weui-cell" style="background: #fff9eb;">
+				<view class="weui-cell__hd">
+					<image src="../../static/images/ic_myapp.png" style="display:block;width:40rpx;height:40rpx;margin-right:14rpx;"></image>
+				</view>
+				<view class="weui-cell__bd">
+					<text style="color: #be9719;font-size: 13px;">点击右上角“添加到我的小程序”，方便下次找到！</text>
+				</view>
+				<view class="weui-cell__ft">
+					<image src="../../static/images/modal_closer.png" style="display: block;width: 15px;height: 15px;"></image>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -14,4 +26,7 @@
 </script>
 
 <style>
+	page {
+		background-color: #fff;
+	}
 </style>
