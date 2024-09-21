1a86bf41a4018d55dc2437740c78c51c2fd68fbc
zyb <zyb2020@qq.com>
2024-09-21 19:46:37 +0800

    服务页面

--- pages/service/index.vue
+++ pages/service/index.vue
@@ -1,22 +1,76 @@
 <template>
 	<view>
+		<view class="od-banner">
+			<image class="od-banner-icon" src="../../static/images/od_bg_icon.png" mode="widthFix"></image>
+			<view class="od-jd od-jd-0">
+				<view class="od-jd-out">
+					<view class="od-jd-in"></view>
+				</view>
+				<view class="vp-flex od-jd-text">
+					<view class="vp-flex_1">
+						<text class="od-jd-st-0">填项订单</text>
+					</view>
+					<view class="vp-flex_1">
+						<text class="od-jd-st-10">在线支付</text>
+					</view>
+					<view class="vp-flex_1">
+						<text class="od-jd-st-20">专人服务</text>
+					</view>
+					<view class="vp-flex_1">
+						<text class="od-jd-st-30">服务完成</text>
+					</view>
+				</view>
+			</view>
+		</view>
 		
+
+		<view class="pub-box">
+			<view class="pub-box-bd">
+				<view class="weui-cell weui-cell_input">
+					<view class="weui-cell__hd">
+						<image class="serv-icon" :src="service.icon_image?service.icon_image_url:'../../static/images/avatar.jpg'"></image>
+					</view>
+					<view class="weui-cell__bd">
+						<text class="serv-name">{{service.name}}</text>
+					</view>
+					<view class="weui-cell__ft">
+						<view class="f5 ic_info" @click="handleTap">服务内容</view>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				
+<script setup>
+	import { onLoad } from '@dcloudio/uni-app'
+	import { ref, reactive, toRaw } from 'vue';
+	const app = getApp()
+	onLoad((options) => {
+		// 获取路由参数
+		console.log(options)
+		main_load(options)
+	})
+	
+	const service = ref({})
+	const main_load = (options)=>{
+		app.globalData.utils.request({
+			url: '/Service/order',
+			data: {
+				svid: options.svid
+			},
+			success: res=>{
+				console.log(res)
+				service.value = res.data.service
 			}
-		},
-		methods: {
-			
-		}
+		})
+	}
+	
+	const handleTap = ()=>{
+		
 	}
 </script>
 
 <style>
-
+@import './index.css';
 </style>
